de7bf7f9b3862ab1c193e3bd7119afca8b413db3
Deepanshu <21bcs072@iiitdmj.ac.in>
2022-12-26 23:50:28 +0530

    added database

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express=require('express');
 const app=express();
-const data=require(__dirname+"/Data.js");
+
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+const itemsSchema = new mongoose.Schema({
+    day:String,
+    title: String,
+    blogs: String
+});
+
+const blog=mongoose.model("blog",itemsSchema);
+
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+};
+const today = new Date();
+const days = today.toLocaleDateString('en-US', options);
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
-var datas=data.function();
+
 app.get("/",(req,res)=>{
-    res.render("index" ,{
-        year: new Date().getFullYear(),
-        data: datas
-    });
+    blog.find({}, (err, foundItems) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('index', { year: new Date().getFullYear(),day: days, data: foundItems });
+        }
+    })
+   
 });
 
 app.get("/about",(req,res)=>{
@@ -27,33 +52,45 @@ app.get("/compose",(req,res)=>{
     res.render("compose" ,{year: new Date().getFullYear()});
 });
 
+
+
 app.post("/compose",(req,res)=>{
-    const title=req.body.title;
-    const blog=req.body.blog;
-    const obj={
-        id: datas.length+1,
-        title: title,
-        blogs: blog
-    }
-    data.function2(obj);
+    const titl=req.body.title;
+    const blogsm=req.body.blog;
+    const newItem = new blog({
+        title: titl,
+        day:days,
+        blogs: blogsm
+    });
+    newItem.save();
     res.redirect("/");
 
 });
 
 app.get("/blogs/:id",(req,res)=>{
     const id=req.params.id;
-    datas.forEach((data)=>{
-        if(data.id==id){
-            res.render("blogs",{
-                year: new Date().getFullYear(),
-                id: data.id,
-                title: data.title,
-                blog: data.blogs
-            });
+    blog.findById(id,(err,resp)=>{
+        if(err){
+            console.log(err);
         }
-    });
+        else{
+            res.render("blogs",{year: new Date().getFullYear(),id:resp._id,days:resp.day,title: resp.title,blog:resp.blogs});
+        }
+    })
 }
 );
+
+app.post('/del',(req,res)=>{
+    const id = req.body.id;
+    blog.findByIdAndRemove(id, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/');
+        }
+    });
+})
 
 app.listen(3000,()=>{
     console.log("Server is running");
